ade9f5d1867c667bdd5fd34cbe196ef61e48c2f3
Eng-Yhassan <muscabhassan81@gmail.com>
2025-07-14 15:37:04 +0300

    update

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const Counter2 = document.querySelector("#Counter2");
 const Counter = document.querySelector("#Counter");
 const incriment = document.querySelector("#incriment");
 const Decriment = document.querySelector("#Decriment");
@@ -6,19 +7,50 @@ const Reset = document.querySelector("#Reset");
 let startValue = 0;
 
 
-
 // incriment
 
 incriment.addEventListener("click", () => {
     startValue++;
     Counter.innerHTML = startValue;
-    // color change when is equal to 10
-    if (startValue === 10) {
-        Counter.style.color = 'red'
+    // // color change when is equal to 10
+    // if (startValue === 10) {
+    //     Counter.style.color = 'red';
+    // }
+    // else {
+    //     Counter.style.color = 'black'
+    // }
+
+
+    //    Creating emoji and color
+    if (startValue == 10) {
+        Counter2.innerHTML = '✅'
+        Counter.style.color = 'red';
+    }
+    else if (startValue == 20) {
+        Counter2.innerHTML = '✌️'
+        Counter.style.color = 'red';
+    }
+    else if (startValue == 30) {
+        Counter2.innerHTML = '😍'
+        Counter.style.color = 'red';
+    }
+    else if (startValue == 40) {
+        Counter2.innerHTML = '🤣'
+        Counter.style.color = 'red';
+    }
+    else if (startValue == 50) {
+        Counter2.innerHTML = '❤️'
+        Counter.style.color = 'red';
+    }
+    else if (startValue == 100) {
+        Counter2.innerHTML = '💕'
+        Counter.style.color = 'red';
     }
     else {
-        Counter.style.color = 'black'
+        Counter2.innerHTML = ''
+        Counter.style.color = 'black';
     }
+
 
 });
 
@@ -32,13 +64,45 @@ Decriment.addEventListener("click", () => {
         startValue = 0;
     }
     Counter.innerHTML = startValue;
-    // color change when is equal to 10
-    if (startValue === 10) {
-        Counter.style.color = 'red'
+    // // color change when is equal to 10
+    // if (startValue === 10) {
+    //     Counter.style.color = 'red'
+    // }
+    // else {
+    //     Counter.style.color = 'black'
+    // }
+
+
+    //    Creating emoji and color
+    if (startValue == 10) {
+        Counter2.innerHTML = '✅'
+        Counter.style.color = 'red';
+    }
+    else if (startValue == 20) {
+        Counter2.innerHTML = '✌️'
+        Counter.style.color = 'red';
+    }
+    else if (startValue == 30) {
+        Counter2.innerHTML = '😍'
+        Counter.style.color = 'red';
+    }
+    else if (startValue == 40) {
+        Counter2.innerHTML = '🤣'
+        Counter.style.color = 'red';
+    }
+    else if (startValue == 50) {
+        Counter2.innerHTML = '❤️'
+        Counter.style.color = 'red';
+    }
+    else if (startValue == 100) {
+        Counter2.innerHTML = '💕'
+        Counter.style.color = 'red';
     }
     else {
-        Counter.style.color = 'black'
+        Counter2.innerHTML = ''
+        Counter.style.color = 'black';
     }
+
 
 });
 
