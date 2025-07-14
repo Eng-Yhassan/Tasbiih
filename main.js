d18e46c2da430445708cbe6f863daff5d1aea01f
Eng-Yhassan <muscabhassan81@gmail.com>
2025-07-14 14:49:33 +0300

    update

--- main.js
+++ main.js
@@ -1,32 +1,48 @@
-const Counter=document.querySelector("#Counter");
-const incriment=document.querySelector("#incriment");
-const Decriment=document.querySelector("#Decriment");
-const Reset=document.querySelector("#Reset");
+const Counter = document.querySelector("#Counter");
+const incriment = document.querySelector("#incriment");
+const Decriment = document.querySelector("#Decriment");
+const Reset = document.querySelector("#Reset");
 
-let startValue=0;
+let startValue = 0;
+
+
 
 // incriment
 
-incriment.addEventListener("click",()=>{
-    startValue ++;
-    Counter.innerHTML=startValue;
+incriment.addEventListener("click", () => {
+    // color change when is equal to 10
+    if (startValue === 9) {
+        Counter.style.color = 'red'
+    }
+    else {
+        Counter.style.color = 'black'
+    }
+    startValue++;
+    Counter.innerHTML = startValue;
 });
 
 
 // Decriment
-Decriment.addEventListener("click",()=>{
-    if(startValue>=1){
-        startValue --
+Decriment.addEventListener("click", () => {
+    if (startValue >= 1) {
+        startValue--
     }
-    else{
-        startValue =0;
+    else {
+        startValue = 0;
     }
-    Counter.innerHTML=startValue;
+    // color change when is equal to 10
+    if (startValue === 10) {
+        Counter.style.color = 'red'
+    }
+    else {
+        Counter.style.color = 'black'
+    }
+    Counter.innerHTML = startValue;
 });
 
 // Reset
-Reset.addEventListener("click",()=>{
-    startValue=0;
+Reset.addEventListener("click", () => {
+    startValue = 0;
 
-    Counter.innerHTML=startValue
+    Counter.innerHTML = startValue
 })
